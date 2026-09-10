8c965dbb6f9472c6b568e8a23b38986d80b54812
Hiten <hiten@Hitens-MacBook-Air.local>
2026-09-11 02:05:31 +0530

    17

---
+++ apps/3_qna_bot_with_groq.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langgraph.checkpoint.memory import MemorySaver
+import streamlit as st
+
+
+
+
+
+
+
+
+
+llm = ChatGroq(model="openai/gpt-oss-120b",streaming=True)
+search = GoogleSerperAPIWrapper()
+tools= [search.run]
+if "memory" not in st.session_state:
+    st.session_state.memory=MemorySaver()
+    st.session_state.history =[]
+
+
+
+
+
+
+agent =create_agent(
+    model=llm,
+    tools=tools,
+    checkpointer=st.session_state.memory,
+    system_prompt="you are amazing ai agent and can search google as well as "
+)
+
+st.subheader("Quick Question/AnswrrChat Bot using groqq")
+
+for message in st.session_state.history:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+query = st.chat_input("enter the message ")
+if query:
+    st.chat_message("user").markdown(query)
+    st.session_state.history.append({"role":"user","content":query})
+
+    respone =agent.stream(
+        {"messages":[{"role":"user","content":query}]},
+        {"configurable":{"thread_id":"1"}},
+        stream_mode='messages'
+
+    )
+
+    ai_container= st.chat_message("ai")
+    with ai_container:
+        space = st.empty()
+        message =""
+        for chunk in respone:
+            message= message+chunk[0].content
+            space.write(message)
+
+        st.session_state.history.append({"role":"ai","content":message})
+
+
+    
--- apps/google_agent.py
+++ apps/google_agent.py
@@ -5,6 +5,7 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 
 
@@ -13,13 +14,15 @@ search= GoogleSerperAPIWrapper()
 
 model = ChatOllama(model = "gemma4:31b-cloud"  )
 
-agent = create_agent(model=model, tools=[search.run],system_prompt="you are a helpful assistant that can answer questions and perform searches using the Google Search API. Use the search tool when necessary to find information.")
+agent = create_agent(model=model, tools=[search.run],system_prompt="you are a helpful assistant that can answer questions and perform searches using the Google Search API. Use the search tool when necessary to find information.",
+                     checkpointer=MemorySaver())
 
 
 while True:
     question = input("Please enter your question: ")
     if(question.lower()=="exit"):
         break
-    response = agent.invoke({"messages":[{"role":"user","content":question}]})
+    response = agent.invoke({"messages":[{"role":"user","content":question}]},
+                            {"configurable":{"thread_id":"1"}})
     print(response['messages'][-1].content)
     print("------------------------------------------------------------")
